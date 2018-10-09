3418c0ec4f1a4f7bdbd4605d6ef522718b0acc2a
Oleksandr Bratashov <abratashov@gmail.com>
2018-10-09 14:56:23 +0300

    Add API call for component.

--- app/javascript/hello_angular/app/app.component.ts
+++ app/javascript/hello_angular/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'hello-angular',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   name = 'Angular!';
+
+  constructor(private http: HttpClient){}
+
+  changeName() {
+    this.http.get('/hello_angular/name').subscribe(data => {
+      this.name = data['name'];
+    });
+  }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'hello_angular#index'
 
   get 'hello_angular/index'
+  get 'hello_angular/name'
 end
